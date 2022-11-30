1007589ec310c5e5b89261d8d7232aae59a3899e
LayK <layton.knight96@gmail.com>
2022-11-29 22:49:18 -0600

    Add files via upload

---
+++ Final Project/4x4.py
@@ -0,0 +1,104 @@
+import math
+import time
+from player4 import HumanPlayer, SmartComputerPlayer, RandomComputerPlayer
+
+
+class TicTacToe():
+    def __init__(self):
+        self.board = self.make_board()
+        self.current_winner = None
+
+    @staticmethod
+    def make_board():
+        return [' ' for _ in range(16)]
+
+    def print_board(self):
+        for row in [self.board[i*4:(i+1) * 4] for i in range(4)]:
+            print('| ' + ' | '.join(row) + ' |')
+
+    @staticmethod
+    def print_board_nums():
+        # 0 | 1 | 2
+        number_board = [[str(i) for i in range(j*4, (j+1)*4)] for j in range(4)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
+
+    def make_move(self, square, letter):
+        if self.board[square] == ' ':
+            self.board[square] = letter
+            if self.winner(square, letter):
+                self.current_winner = letter
+            return True
+        return False
+
+    def winner(self, square, letter):
+        # check the row
+        row_ind = math.floor(square / 4)
+        row = self.board[row_ind*4:(row_ind+1)*4]
+        # print('row', row)
+        if all([s == letter for s in row]):
+            return True
+        col_ind = square % 4
+        column = [self.board[col_ind+i*4] for i in range(4)]
+        # print('col', column)
+        if all([s == letter for s in column]):
+            return True
+        if square % 5 == 0:
+            diagonal1 = [self.board[i] for i in [0, 5, 10, 11]]
+            # print('diag1', diagonal1)
+            if all([s == letter for s in diagonal1]):
+                return True
+        if square % 3 == 0:
+            diagonal2 = [self.board[i] for i in [3, 6, 9, 12]]
+            # print('diag2', diagonal2)
+            if all([s == letter for s in diagonal2]):
+                return True
+        return False
+
+    def empty_squares(self):
+        return ' ' in self.board
+
+    def num_empty_squares(self):
+        return self.board.count(' ')
+
+    def available_moves(self):
+        return [i for i, x in enumerate(self.board) if x == " "]
+
+
+def play(game, x_player, o_player, print_game=True):
+
+    if print_game:
+        game.print_board_nums()
+
+    letter = 'X'
+    while game.empty_squares():
+        if letter == 'O':
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+        if game.make_move(square, letter):
+
+            if print_game:
+                print(letter + ' makes a move to square {}'.format(square))
+                game.print_board()
+                print('')
+
+            if game.current_winner:
+                if print_game:
+                    print(letter + ' wins!')
+                return letter  # ends the loop and exits the game
+            letter = 'O' if letter == 'X' else 'X'  # switches player
+
+        time.sleep(.8)
+    
+
+    if print_game:
+        print('It\'s a tie!')
+
+
+
+if __name__ == '__main__':
+    x_player = SmartComputerPlayer('X')
+    o_player = RandomComputerPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game=True)
---
+++ Final Project/player4.py
@@ -0,0 +1,121 @@
+import math
+import time
+import random
+import sys 
+
+sys.setrecursionlimit(1500)
+
+#increase or decrease to determine the amount of nodes to search before giving up and picking the best score. This affects how fast the code runs
+cutoff = 400000
+
+class Player():
+    def __init__(self, letter):
+        self.letter = letter
+
+    def get_move(self, game):
+        pass
+
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+        
+
+
+    def get_move(self, game):
+        valid_square = Falseval = None
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-15): ')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again.')
+        return val
+
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+
+
+class SmartComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+        self.lastmove = -1
+        self.maxnodes = cutoff
+        self.limit = False  #see if we hit our cutoff
+        self.currnodes = 0  #total nodes recursed
+        self.maxprune = 0   #number of max prunings
+        self.minprune = 0   #number of min prunings
+        self.turn = 0
+        self.nextBestMove = {'position': None, 'score': 0}
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 16:
+            square = random.choice(game.available_moves())
+        else:
+            self.currnodes = 0  #reset value
+            self.maxprune = 0   #reset value
+            self.minprune = 0   #reset value
+            self.nextBestMove = {'position': None, 'score': 0}
+            t0 = time.time()
+            square = self.minimax(game, self.letter)['position']
+            t1 = time.time()
+            if(self.currnodes > cutoff):
+                print('Cutoff reached')
+            print('The time minimax took to select a move:', t1-t0)
+            print('Number of Nodes generated:', self.currnodes +1)
+            print('Number of Max prunings: ', self.maxprune)
+            print('Number of Min prunings: ', self.minprune)
+        return square
+
+    def minimax(self, state, player):
+        max_player = self.letter  # yourself
+        other_player = 'O' if player == 'X' else 'X'
+        # first we want to check if the previous move is a winner
+        if state.current_winner == other_player:
+            return {'position': None, 'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (state.num_empty_squares() + 1)}
+        elif not state.empty_squares():
+            return {'position': None, 'score': 0}
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}  # each score should maximize
+        else:
+            best = {'position': None, 'score': math.inf}  # each score should minimize
+
+        if(self.currnodes < self.maxnodes):
+            for possible_move in state.available_moves():
+                state.make_move(possible_move, player)
+                self.currnodes += 1
+                #if(self.currnodes >cutoff):
+                #   self.Limit = True       # we have hit our limit
+                    
+                
+                sim_score = self.minimax(state, other_player)  # simulate a game after making that move
+
+                # undo move
+                state.board[possible_move] = ' '
+                state.current_winner = None
+                sim_score['position'] = possible_move  # this represents the move optimal next move
+
+                if player == max_player:  # X is max player
+                    if sim_score['score'] > best['score']:
+                        self.maxprune += 1
+                        best = sim_score
+                else:
+                    if sim_score['score'] < best['score']:
+                        self.minprune += 1
+                        best = sim_score
+                        
+        self.nextBestMove = best
+        return self.nextBestMove
+    
+
+        
